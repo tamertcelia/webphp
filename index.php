dc96710c21ee42bf793e342dd211a67b02e939eb
tamert celia <tamertcelia75@gmail.com>
2023-08-19 09:18:32 +0000

    Add modification1 de index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>Test PHP</title>
  </head>
  <body>
- <?php echo '<p>Bonjour le monde githubbbbbb</p>'; ?>
+ <?php echo '<p>Bonjour1 le monde githubbbbbb</p>'; ?>
+<?php echo '<p>Bonjour2 le monde githubbbbbb</p>'; ?>
  </body>
 </html>
